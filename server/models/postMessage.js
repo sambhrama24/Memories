fd1d681e9db19d8360ef544a2c6259f6435f20bd
sambhrama24 <sambhramapatel@gmail.com>
2021-06-21 18:00:18 +0530

    Initial commit

---
+++ server/models/postMessage.js
@@ -0,0 +1,20 @@
+import mongooose from 'mongoose';
+ const postSchema = mongooose.Schema({
+     title : String,
+     message : String,
+     creator : String,
+     tags : [String],
+     selectedFile : String,
+     likeCount : {
+         type : Number,
+         default : 0
+     },
+     createdAt: {
+         type: Date(),
+         default: new Date()
+     },
+ });
+
+ const PostMessage = mongooose.model('PostMessage', postSchema);
+
+ export default PostMessage;
